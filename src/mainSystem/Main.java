9f2f4dd84e85928c4f1a64f1f99197533cdb24cd
stillstargazer <35256047+stillstargazer@users.noreply.github.com>
2018-02-05 17:11:44 +0800

    实验室选择
    finish

--- src/mainSystem/Main.java
+++ src/mainSystem/Main.java
@@ -1,5 +1,28 @@
 package mainSystem;
 
-public class Main {
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
+import body.ReagentList;
+import window.MainWindow;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		Object[] obj2 ={ "413", "411"};  
+		String s = (String) JOptionPane.showInputDialog(null,"请选择您的实验室：\n", "实验室选择", JOptionPane.PLAIN_MESSAGE, null, obj2, "足球");
+		File f = new File("./"+s+".txt");
+		try
+		{
+			ReagentList rl = new ReagentList(f);
+			MainWindow mw = new MainWindow(rl);
+		} catch (NumberFormatException | IOException e)
+		{
+			// TODO 自动生成的 catch 块
+			JOptionPane.showMessageDialog(null, "并没有您实验室的数据！", "错误",JOptionPane.ERROR_MESSAGE);
+		}
+	}
 }

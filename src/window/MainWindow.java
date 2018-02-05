8695e131c4aae9265b67cd4f25cda7c3d0269cc6
stillstargazer <35256047+stillstargazer@users.noreply.github.com>
2018-02-05 16:34:35 +0800

    Search part
    finish

--- src/window/MainWindow.java
+++ src/window/MainWindow.java
@@ -39,111 +39,106 @@ public class MainWindow extends JFrame
 	private JButton delete_reagent;
 	private JButton set_reagent;
 	private JButton find_reagent;
-	
+
 	private AdditionWindow aw;
-	
+
 	private Font buttonFont = new Font("微软雅黑", Font.BOLD, 30);
 	private Font tableFont = new Font("微软雅黑", Font.PLAIN, 25);
 	private Font tableHeaderFont = new Font("微软雅黑", Font.PLAIN, 28);
-	
-	
-	
 
-	
 	public MainWindow(ReagentList rl)
 	{
 		super("试剂管理系统");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1000, 600);
 		getContentPane().setLayout(new BorderLayout(0, 0));
-		
 
-		
 		functionPane = new JPanel();
 		getContentPane().add(functionPane, BorderLayout.NORTH);
 		functionPane.setLayout(new GridLayout(1, 4, 0, 0));
-		
+
 		add_reagent = new JButton("\u6DFB\u52A0\u836F\u54C1");
 		add_reagent.setFont(buttonFont);
 		functionPane.add(add_reagent);
-		
+
 		delete_reagent = new JButton("\u5220\u9664\u836F\u54C1");
 		delete_reagent.setFont(buttonFont);
 		functionPane.add(delete_reagent);
-		
+
 		set_reagent = new JButton("\u4FEE\u6539\u836F\u54C1");
 		set_reagent.setFont(buttonFont);
 		functionPane.add(set_reagent);
-		
+
 		find_reagent = new JButton("\u67E5\u8BE2\u836F\u54C1");
 		find_reagent.setFont(buttonFont);
 		functionPane.add(find_reagent);
-		
-		TblModel tblmodel = new TblModel(rl);              //表格模型，负责更新reagentTable中的数据
+
+		TblModel tblmodel = new TblModel(rl); // 表格模型，负责更新reagentTable中的数据
 		tblmodel.addTableModelListener(tblmodel);
-		
-		JTable reagentTable = new JTable(tblmodel);        
-		
+
+		JTable reagentTable = new JTable(tblmodel);
+
 		reagentTable.setShowVerticalLines(false);
 		reagentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		reagentTable.setFont(tableFont);
 		reagentTable.setRowHeight(40);
-		
-		//悬浮窗
+
+		// 悬浮窗
 		reagentTable.addMouseMotionListener(new MouseAdapter()
 		{
 			public void mouseMoved(MouseEvent e)
 			{
-				//TODO 是只显示完整中文名、英文名，还是显示所有字段？？？
+				// TODO 是只显示完整中文名、英文名，还是显示所有字段？？？
 				int row = reagentTable.rowAtPoint(e.getPoint());
 				int col = reagentTable.columnAtPoint(e.getPoint());
-				if(col == 0)
+				if (col == 0)
 				{
 					reagentTable.setToolTipText(rl.get(row).getCHIname());
 				}
 			}
 		});
-		
-		//数字居中
+
+		// 数字居中
 		DefaultTableCellRenderer reagentTableCellRender = new DefaultTableCellRenderer();
-		reagentTableCellRender.setHorizontalAlignment(JLabel.CENTER);;
+		reagentTableCellRender.setHorizontalAlignment(JLabel.CENTER);
+		;
 		reagentTable.setDefaultRenderer(Number.class, reagentTableCellRender);
-		
+
 		JScrollPane reagentPane = new JScrollPane(reagentTable);
 		reagentPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		getContentPane().add(reagentPane, BorderLayout.CENTER);
 
-		//表头
+		// 表头
 		JTableHeader reagentPaneHeader = reagentTable.getTableHeader();
 		reagentPaneHeader.setReorderingAllowed(false);
 		reagentPaneHeader.setResizingAllowed(false);
 		reagentPaneHeader.setFont(tableHeaderFont);
-		
-		//ActionListener
+
+		// ActionListener
 		add_reagent.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				//System.out.println("hahaha");
-				if(e.getSource() == add_reagent)
+				// System.out.println("hahaha");
+				if (e.getSource() == add_reagent)
 				{
-					
+
 					aw = new AdditionWindow(rl);
-				}				
-			} 
-			
+				}
+			}
+
 		});
-		
+
 		delete_reagent.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				//TODO 没有想好应该实现成什么样
-				if(e.getSource() == delete_reagent)
+				// TODO 没有想好应该实现成什么样
+				if (e.getSource() == delete_reagent)
 				{
 					try
 					{
@@ -155,43 +150,33 @@ public class MainWindow extends JFrame
 						// TODO 自动生成的 catch 块
 						e1.printStackTrace();
 					}
-					
-				}				
+
+				}
 			}
 		});
-		
+
 		set_reagent.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				EditWindow ew = new EditWindow();				
+				EditWindow ew = new EditWindow();
 			}
 		});
-		
+
 		find_reagent.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				SearchWindow sw = new SearchWindow();			
+				SearchWindow sw = new SearchWindow(rl, reagentTable);
 			}
 		});
-		
+
 		this.setVisible(true);
-		
+
 	}
 
-	
 }
-
-
-
-
-
-
-
-
-
--- src/window/SearchWindow.java
+++ src/window/SearchWindow.java
@@ -1,5 +1,42 @@
 package window;
 
-public class SearchWindow {
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
 
+import body.ReagentList;
+
+public class SearchWindow
+{
+	public SearchWindow(ReagentList rl, JTable tbl)
+	{
+		Object[] options ={ "中文名", "英文名","CAS" };  //自定义按钮上的文字
+		int m = JOptionPane.showOptionDialog(null, "请选择检索方式？", "提示",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (m==0)
+		{
+			String temp=JOptionPane.showInputDialog(null,"请输入中文名：\n","修改药品",JOptionPane.PLAIN_MESSAGE); 
+			int n=rl.findCHIname(temp);
+			if (n==-1)
+				JOptionPane.showMessageDialog(null, "没有找到对应药品！", "错误",JOptionPane.ERROR_MESSAGE);
+			else	
+				tbl.setRowSelectionInterval(n, n);
+		}
+		if (m==1)
+		{
+			String temp=JOptionPane.showInputDialog(null,"请输入英文名：\n","修改药品",JOptionPane.PLAIN_MESSAGE); 
+			int n=rl.findENGname(temp);
+			if (n==-1)
+				JOptionPane.showMessageDialog(null, "没有找到对应药品！", "错误",JOptionPane.ERROR_MESSAGE);
+			else	
+				tbl.setRowSelectionInterval(n, n);
+		}
+		if (m==2)
+		{
+			String temp=JOptionPane.showInputDialog(null,"请输入CAS：\n","修改药品",JOptionPane.PLAIN_MESSAGE); 
+			int n=rl.findCAS(temp);
+			if (n==-1)
+				JOptionPane.showMessageDialog(null, "没有找到对应药品！", "错误",JOptionPane.ERROR_MESSAGE);
+			else	
+				tbl.setRowSelectionInterval(n, n);
+		}
+	}
 }
